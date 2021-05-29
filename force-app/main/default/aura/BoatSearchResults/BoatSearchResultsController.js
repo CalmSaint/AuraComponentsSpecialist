02c364bfbf1cd8796db700aa703100343874f39f
CalmSaint <Kudixit@deloitte.com>
2021-05-29 22:03:22 +0530

    Highlight the selected boat

--- force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
+++ force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -6,5 +6,9 @@
     doSearch: function(component,event,helper){
         component.set('v.boatTypeId',event.getParam('arguments').boatTypeId);
         helper.onSearch(component,event);
+    },
+
+    onBoatSelect: function(component,event,helper){
+        component.set('v.selectedBoatId',event.getParam('boatId'));
     }
 })
--- force-app/main/default/aura/BoatTile/BoatTileController.js
+++ force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -1,5 +1,7 @@
 ({
-    myAction : function(component, event, helper) {
-
+    onBoatClick : function(component, event, helper) {
+        let boatSelect = component.getEvent('BoatSelect');
+        boatSelect.setParams({boatId : component.get('v.boat.Id')});
+        boatSelect.fire();
     }
 })
